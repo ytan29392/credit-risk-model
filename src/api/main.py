1016a509d3f5aa2df5f7dc65f430f5d46b7a73da
ytan29392 <ytan29392@gmail.com>
2025-07-01 21:19:57 +0300

    add: main src

---
+++ src/api/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from src.api.pydantic_models import CustomerInput, RiskPrediction
+import mlflow.pyfunc
+
+app = FastAPI()
+
+# Load model from MLflow Registry
+model = mlflow.pyfunc.load_model("models:/best_credit_model/Production")
+
+@app.post("/predict", response_model=RiskPrediction)
+def predict_risk(data: CustomerInput):
+    input_df = data.to_df()
+    prediction = model.predict(input_df)
+    return RiskPrediction(probability=float(prediction[0]))
